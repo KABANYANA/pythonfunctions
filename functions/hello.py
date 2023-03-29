75e9e9a7df775321838045559d9dec05ceabfb2f
KABANYANA <joyeusekabanyana@gmail.com>
2023-03-29 23:02:40 +0300

    new

---
+++ functions/hello.py
@@ -0,0 +1,7 @@
+def hello(name):
+    print(f"Hello {name}")
+
+def welcome(student,school):
+    print(f"welcome to {school}, {student}")
+
+  
---
+++ functions/math.py
@@ -0,0 +1,23 @@
+
+
+# addition
+def add(a,b):
+    answer =a+b
+    return answer
+# substraction
+def sub(a,b):
+    answer1 =a-b
+    return answer1
+
+# multiplication
+def mult(a,b):
+    answer2 =a*b
+    return answer2
+# division
+def divide(a,b):
+    answer3 =a/b
+    return answer3
+# remainder
+def modulus(a,b):
+    answer4 =a%b
+    return answer4
